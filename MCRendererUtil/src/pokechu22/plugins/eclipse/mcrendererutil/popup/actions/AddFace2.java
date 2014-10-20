efef3495c04ee02630144054290c8f99d764b575
Pokechu22 <pokechu022@gmail.com>
2014-10-20 14:23:20 -0700

    Sucessfully getting methods now!

--- MCRendererUtil/src/pokechu22/plugins/eclipse/mcrendererutil/popup/actions/AddFace2.java
+++ MCRendererUtil/src/pokechu22/plugins/eclipse/mcrendererutil/popup/actions/AddFace2.java
@@ -2,17 +2,19 @@ package pokechu22.plugins.eclipse.mcrendererutil.popup.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.jdt.core.IMethod;
 
 import pokechu22.plugins.eclipse.mcrendererutil.ui.AdvancedAddFaceDialog;
 
 public class AddFace2 implements IObjectActionDelegate {
 
 	private Shell shell;
+	
+	private IMethod currentMethod;
 	
 	/**
 	 * Constructor for Action1.
@@ -39,7 +41,20 @@ public class AddFace2 implements IObjectActionDelegate {
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
+		if (!(selection instanceof IStructuredSelection)) {
+			action.setEnabled(false);
+			return;
+		}
+		IStructuredSelection sel = (IStructuredSelection) selection;
 		
+		if (!(sel.getFirstElement() instanceof IMethod)) {
+			//Only handles IMethods.
+			action.setEnabled(false);
+			return;
+		}
+		
+		action.setEnabled(true);
+		this.currentMethod = (IMethod) sel.getFirstElement();
 	}
 
 }
